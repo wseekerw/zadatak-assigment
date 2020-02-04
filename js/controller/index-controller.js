73956873bbd65352482ff75ed0c76785495bfce7
WSeeker <tasic.lazar@gmail.com>
2020-02-04 14:38:45 +0100

    Offers API request and elements finished

--- js/controller/index-controller.js
+++ js/controller/index-controller.js
@@ -3,4 +3,24 @@ var app = angular.module('zadatak');
 app.controller("indexController", function($scope, $http){
 
 
+    var offersPromise = $http({
+        url: 'https://selfcare-service.demo.melita.com/interview/api/offers',
+        method: "get"
+    })
+
+    offersPromise.then(function(response){
+        console.log(response)
+        $scope.offers = response.data.offers;
+    })
+
+    // var subscriptionsPromise = $http({
+    //     url: 'https://selfcare-service.demo.melita.com/interview/api/offers/100/subscriptions',
+    //     method: "get"
+    // })
+
+    // subscriptionsPromise.then(function(response){
+    //     console.log(response)
+
+    // })
+
 })
